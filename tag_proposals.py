a035ac31661b5e67560eb8559f2c5f83f063119d
Marek Sucharda <m.sucharda@email.cz>
2025-11-23 14:04:20 +0100

    config

--- tag_proposals.py
+++ tag_proposals.py
@@ -1,6 +1,9 @@
+import yaml
+import json
+from typing import List
+from uuid import UUID
 from pydantic import BaseModel
 from uuid import UUID
-
 
 
 class TextWithSpan(BaseModel):
@@ -8,11 +11,13 @@ class TextWithSpan(BaseModel):
     span_start: int | None = None
     span_end: int | None = None
 
+
 class Tag(BaseModel):
     id: UUID
     name: str
     description: str | None = None
     examples: list[TextWithSpan] | None = None
+
 
 class TagSpanProposal(BaseModel):
     tag_id: UUID
@@ -21,12 +26,14 @@ class TagSpanProposal(BaseModel):
     confidence: float | None = None
     reason: str | None = None
 
+
 class DBRequest(BaseModel):
-    collection_id: UUID  | None = None
+    collection_id: UUID | None = None
+
 
 class DBSearch:
     pass
-    def search_texts(self, query: str) -> list[str]:
+
 
 class TextChunk(BaseModel):
     id: UUID
@@ -36,26 +43,26 @@ class TextChunk(BaseModel):
 class TextChunkWithTags(TextChunk):
     tags: list[TagSpanProposal]
 
-"""
-config: .yaml
-- DB config 
--- db type
--- connection string
--- ...
 
-api key, c
+class Config:
+    def __init__(self, config_file: str):
+        with open(config_file, 'r') as f:
+            cfg = yaml.safe_load(f)
 
-"""
+        self.cfg = cfg
+        print('config: ' + json.dumps(self.cfg, indent=2))
+
 
 class TagProposal:
     def __init__(self, config_file: str):
-        pass
+        self.config = Config(config_file)
 
     async def propose_tags(self, text: TextChunk, tag: list[Tag]) -> TextChunkWithTags:
-        pass
+        return TextChunkWithTags(id=text.id, text=text.text, tags=[])
 
     async def propose_tags_in_db(self, tag: Tag,  db_request: DBRequest) -> list[TextChunkWithTags]:
-        """
+        return []
 
-        """
-        pass
+
+if __name__ == "__main__":
+    tag_proposal = TagProposal("config.yaml")
