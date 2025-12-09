f480e5b07fe4e1fbae1cfd3b735415cd5fd6187b
Marek Sucharda <m.sucharda@email.cz>
2025-12-09 21:47:33 +0100

    remove weaviate

--- myclass.py
+++ myclass.py
@@ -1,43 +1,17 @@
 import requests
-import weaviate
-from weaviate.classes.query import MetadataQuery
-from typing import List, Dict, Any
+from typing import List
 
 
-class MyClass:
+class Embedder:
     def __init__(
         self,
         embedding_service_url: str,
         embedding_endpoint: str,
-        weaviate_port: int,
-        weaviate_grpc_port: int,
         collection_name: str
     ):
         self.embedding_service_url = embedding_service_url
         self.embedding_endpoint = embedding_endpoint
         self.collection_name = collection_name
-
-        # připojení k DB
-        try:
-            self.client = weaviate.connect_to_local(
-                port=weaviate_port,
-                grpc_port=weaviate_grpc_port
-            )
-
-        except Exception as e:
-            raise ConnectionError(f"Nepodařilo se připojit k Weaviate: {e}")
-
-        # kontrola existence kolekce
-        if not self.client.collections.exists(self.collection_name):
-            self.close()
-            raise ValueError(
-                f"Kolekce '{self.collection_name}' neexistuje"
-            )
-
-    def close(self):
-        """Uzavře spojení s databází."""
-        if self.client.is_connected():
-            self.client.close()
 
     def get_embedding(self, text: str) -> List[float]:
         """Získá vektor voláním BE služby."""
@@ -53,61 +27,24 @@ class MyClass:
 
         except Exception as e:
             raise ConnectionError(
-                f"Volání embedding služby ({url}) selhalo: {e}")
-
-    def find_relevant_chunks(self, vector: List[float], limit: int = 1000) -> List[Dict[str, Any]]:
-        """najde {limit} nejbližších chunků ve Weaviate db k zadanému popisu tagu"""
-        try:
-            collection = self.client.collections.get(self.collection_name)
-
-            response = collection.query.near_vector(
-                near_vector=vector,
-                limit=limit,
-                return_metadata=MetadataQuery().full(),  # full vraci všechna metadata
-            )
-
-            results = []
-            for obj in response.objects:
-                results.append({
-                    "id": str(obj.uuid),
-                    "text": obj.properties['text'],
-                    "distance": obj.metadata.distance,
-                    "metadata": obj.metadata.__dict__
-                })
-
-            return results
-
-        except Exception as e:
-            print(f"Chyba při dotazu do Weaviate: {e}")
-            return []
+                f"Volání embedding služby {url} selhalo: {e}")
 
 
 if __name__ == "__main__":
     import json
 
-    mc = MyClass(
+    embedding_service = Embedder(
         embedding_service_url="http://localhost:9001",
         embedding_endpoint="/embed_query",
-        weaviate_port=9000,
-        weaviate_grpc_port=50055,
         collection_name="Chunks"
     )
 
     try:
-        embedding = mc.get_embedding(
+        embedding = embedding_service.get_embedding(
             text="Bezpečnost v ulicích, kamery a policejní vyšetřování",
         )
-        chunks = mc.find_relevant_chunks(
-            vector=embedding,
-            limit=1
-        )
 
-        for chunk in chunks:
-            chunk['text'] = chunk['text'][:30]
-            print(json.dumps(chunk, indent=2, default=str, ensure_ascii=False))
+        print(json.dumps(embedding[:20], indent=2, ensure_ascii=False))
 
     except Exception as e:
         print(f"Chyba: {e}")
-
-    finally:
-        mc.close()
