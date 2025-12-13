8cc980788eb4595c2c819c7d11e9c802b7f4e903
Martin Kostelnik <martinkostelnik0@gmail.com>
2025-12-13 16:59:45 +0100

    Prepare skeleton for Cross BERT topicer

--- topicer/__init__.py
+++ topicer/__init__.py
@@ -4,6 +4,7 @@ from .tagging.tag_proposal_v2 import TagProposalV2
 from .tagging.gliner import GlinerTopicer
 from .llm import OpenAsyncAPI, OllamaAsyncAPI
 from .embedding import LocalEmbedder
+from .tagging.cross_bert import CrossBertTopicer
 
 __all__ = [
     "factory",
