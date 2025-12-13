f1e1aa1c619ce19d45fe754f0e8458d4a8de2913
Martin Kišš <xkissm00@stud.fit.vutbr.cz>
2025-12-13 19:14:28 +0100

    Implement routes

--- topicer_api/routes/topicer_routes.py
+++ topicer_api/routes/topicer_routes.py
@@ -1,10 +1,13 @@
+import os
 from typing import Sequence
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 from fastapi.responses import RedirectResponse
-from classconfig import Config
 
 from topicer import factory
 from topicer.schemas import DBRequest, Tag, TextChunk
+
+from topicer_api.config import config as app_config
+
 
 topicer_router = APIRouter()
 
@@ -16,76 +19,84 @@ async def root():
 
 @topicer_router.get("/configs")
 async def get_configs():
-    return []
+    configs = [os.path.splitext(file)[0] for file in os.listdir(app_config.TOPICER_CONFIGS_DIR)
+               if file.endswith(app_config.TOPICER_CONFIGS_EXTENSION)]
+    return configs
 
 
-@topicer_router.get("/discover_topics_sparse")
-async def discover_topics_sparse(config: str | dict | Config, texts: Sequence[TextChunk], n: int | None = None):
-    topicer_model = factory(config)
+@topicer_router.post("/discover_topics_sparse")
+async def discover_topics_sparse(config_name: str, texts: Sequence[TextChunk], n: int | None = None):
+    config_path = str(os.path.join(app_config.TOPICER_CONFIGS_DIR, config_name + app_config.TOPICER_CONFIGS_EXTENSION))
+    topicer_model = factory(config_path)
 
     try:
         result = await topicer_model.discover_topics_sparse(texts=texts, n=n)
     except NotImplementedError:
-        result = {"message": "Not implemented"}
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Method not applicable to {config_name}.")
 
     return result
 
 
-@topicer_router.get("/discover_topics_dense")
-async def discover_topics_dense(config: str | dict | Config, texts: Sequence[TextChunk], n: int | None = None):
-    topicer_model = factory(config)
+@topicer_router.post("/discover_topics_dense")
+async def discover_topics_dense(config_name: str, texts: Sequence[TextChunk], n: int | None = None):
+    config_path = str(os.path.join(app_config.TOPICER_CONFIGS_DIR, config_name + app_config.TOPICER_CONFIGS_EXTENSION))
+    topicer_model = factory(config_path)
 
     try:
         result = await topicer_model.discover_topics_dense(texts=texts, n=n)
     except NotImplementedError:
-        result = {"message": "Not implemented"}
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Method not applicable to {config_name}.")
 
     return result
 
 
-@topicer_router.get("/discover_topics_in_db_sparse")
-async def discover_topics_in_db_sparse(config: str | dict | Config, db_request: DBRequest, n: int | None = None):
-    topicer_model = factory(config)
+@topicer_router.post("/discover_topics_in_db_sparse")
+async def discover_topics_in_db_sparse(config_name: str, db_request: DBRequest, n: int | None = None):
+    config_path = str(os.path.join(app_config.TOPICER_CONFIGS_DIR, config_name + app_config.TOPICER_CONFIGS_EXTENSION))
+    topicer_model = factory(config_path)
 
     try:
         result = await topicer_model.discover_topics_in_db_sparse(db_request=db_request, n=n)
     except NotImplementedError:
-        result = {"message": "Not implemented"}
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Method not applicable to {config_name}.")
 
     return result
 
 
-@topicer_router.get("/discover_topics_in_db_dense")
-async def discover_topics_in_db_dense(config: str | dict | Config, db_request: DBRequest, n: int | None = None):
-    topicer_model = factory(config)
+@topicer_router.post("/discover_topics_in_db_dense")
+async def discover_topics_in_db_dense(config_name: str, db_request: DBRequest, n: int | None = None):
+    config_path = str(os.path.join(app_config.TOPICER_CONFIGS_DIR, config_name + app_config.TOPICER_CONFIGS_EXTENSION))
+    topicer_model = factory(config_path)
 
     try:
         result = await topicer_model.discover_topics_in_db_dense(db_request=db_request, n=n)
     except NotImplementedError:
-        result = {"message": "Not implemented"}
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Method not applicable to {config_name}.")
 
     return result
 
 
-@topicer_router.get("/propose_tags")
-async def propose_tags(config: str | dict | Config, text_chunk: TextChunk, tags: list[Tag]):
-    topicer_model = factory(config)
+@topicer_router.post("/propose_tags")
+async def propose_tags(config_name: str, text_chunk: TextChunk, tags: list[Tag]):
+    config_path = str(os.path.join(app_config.TOPICER_CONFIGS_DIR, config_name + app_config.TOPICER_CONFIGS_EXTENSION))
+    topicer_model = factory(config_path)
 
     try:
         result = await topicer_model.propose_tags(text_chunk=text_chunk, tags=tags)
     except NotImplementedError:
-        result = {"message": "Not implemented"}
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Method not applicable to {config_name}.")
 
     return result
 
 
-@topicer_router.get("/propose_tags_in_db")
-async def propose_tags_in_db(config: str | dict | Config, tag: Tag, db_request: DBRequest):
-    topicer_model = factory(config)
+@topicer_router.post("/propose_tags_in_db")
+async def propose_tags_in_db(config_name: str, tag: Tag, db_request: DBRequest):
+    config_path = str(os.path.join(app_config.TOPICER_CONFIGS_DIR, config_name + app_config.TOPICER_CONFIGS_EXTENSION))
+    topicer_model = factory(config_path)
 
     try:
         result = await topicer_model.propose_tags_in_db(tag=tag, db_request=db_request)
     except NotImplementedError:
-        result = {"message": "Not implemented"}
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Method not applicable to {config_name}.")
 
     return result
