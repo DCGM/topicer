7f0aab89f94604eac393f60989c8504f49e35eef
Martin Kišš <xkissm00@stud.fit.vutbr.cz>
2025-12-15 18:46:59 +0100

    Add lifespan function with topicers loading

--- topicer_api/main.py
+++ topicer_api/main.py
@@ -1,6 +1,21 @@
+import logging
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
 from topicer_api.routes import topicer_router
+from topicer_api.topicers import load_topicers
 
-app = FastAPI()
+
+logger = logging.getLogger(__name__)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.loaded_topicers = load_topicers()
+    logger.info("Topicer API started and topicer configurations loaded.")
+    yield
+    logging.info("Shutting down Topicer API...")
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(topicer_router, prefix="/v1")
