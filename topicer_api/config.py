ca5053255ca7100d5ffe79aea2832efef043c725
Martin Kišš <xkissm00@stud.fit.vutbr.cz>
2025-12-15 17:06:30 +0100

    Update default API config values

--- topicer_api/config.py
+++ topicer_api/config.py
@@ -5,10 +5,10 @@ TRUE_VALUES = {"true", "1"}
 class Config:
     def __init__(self):
         self.APP_HOST = os.getenv("APP_HOST", "127.0.0.1")
-        self.APP_PORT = int(os.getenv("APP_PORT", "8666"))
+        self.APP_PORT = int(os.getenv("APP_PORT", "8000"))
         self.APP_RELOAD = self._env_bool("APP_RELOAD", False)
 
-        self.TOPICER_API_CONFIGS_DIR = os.getenv("TOPICER_API_CONFIGS_DIR", "configs")
+        self.TOPICER_API_CONFIGS_DIR = os.getenv("TOPICER_API_CONFIGS_DIR", "./configs")
         self.TOPICER_API_CONFIGS_EXTENSION = os.getenv("TOPICER_API_CONFIGS_EXTENSION", ".yaml")
 
     @staticmethod
