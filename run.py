b35a0ff6c83043f06c9e03273de8048b094e7949
Richard Juřica <richard.juricabtc@gmail.com>
2026-01-29 06:50:22 +0100

    demo(run): Add example for `propose_tags_in_db` functionality

--- run.py
+++ run.py
@@ -4,6 +4,10 @@ import logging
 from tests.test_data import tag1, tag2, tag3, text_chunk
 from topicer.schemas import TextChunkWithTagSpanProposals
 from topicer.base import factory
+from topicer.embedding.default_service import DefaultEmbeddingService
+from topicer.database.weaviate_service import WeaviateService
+from topicer.schemas import Tag
+from uuid import uuid4
 
 
 def print_tag_proposals_with_spans(proposals: TextChunkWithTagSpanProposals):
@@ -30,12 +34,26 @@ async def main():
     # načtení API klíče
     load_dotenv()
 
-    # Factory načte API klíč automaticky z prostředí
-    tag_proposal = factory("config.yaml")
+    # # Factory načte API klíč automaticky z prostředí
+    topicer = factory("config.yaml")
+    
+    # tag: Tag = Tag(
+    #     id=uuid4(),
+    #     name="Jaká byla návštěva v Belgii v Antverpách v atletických mezinarodních závodech?",
+    # )
+    tag: Tag = Tag(
+        id=uuid4(),
+        name="Jak se jmenovalo trojsvazkové dílo, které sjednocovalo soustau měr?",
+    )
+    
+    # proposals: TextChunkWithTagSpanProposals = await topicer.propose_tags(text_chunk, [tag1, tag2, tag3])
+    proposals = await topicer.propose_tags_in_db(tag, None)
+    
+    print(f"\nCelkem nalezeno {len(proposals)} textů s návrhy tagů v DB.\n")
 
-    proposals: TextChunkWithTagSpanProposals = await tag_proposal.propose_tags(text_chunk, [tag1, tag2, tag3])
-
-    print_tag_proposals_with_spans(proposals)
+    for proposals in proposals:
+        print_tag_proposals_with_spans(proposals)
+        
 
 if __name__ == "__main__":
     import asyncio
