cd19d8a40b4b4a2f48e89f333fcacffb539c6f84
Richard Juřica <richard.juricabtc@gmail.com>
2026-02-09 14:22:42 +0100

    refactor(tests): Use shared 'matcher' fixture in test_fuzzy_matcher.py

--- tests/test_utils/test_fuzzy_matcher.py
+++ tests/test_utils/test_fuzzy_matcher.py
@@ -1,10 +1,4 @@
 import pytest
-from topicer.utils.fuzzy_matcher import FuzzyMatcher
-
-
-@pytest.fixture
-def matcher():
-    return FuzzyMatcher(max_dist_ratio=0.2)
 
 
 @pytest.mark.parametrize("input_text, expected", [
