e1592c4cb968c17085776407b78d268c2705d7d2
Martin Dočekal <docekmartin@gmail.com>
2026-04-16 16:12:44 +0200

    adding connect and close for topicers using db_connection type hint for LoadedTopics

--- topicer_api/main.py
+++ topicer_api/main.py
@@ -2,6 +2,7 @@ import logging
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
+from topicer.base import BaseDBConnection
 from topicer_api.routes import topicer_router
 from topicer_api.topicers import load_topicers
 
@@ -13,7 +14,18 @@ logger = logging.getLogger(__name__)
 async def lifespan(app: FastAPI):
     app.state.loaded_topicers = load_topicers()
     logger.info("TopicerAPI started and topicer configurations loaded.")
+
+    for topicer in app.state.loaded_topicers.values():
+        if hasattr(topicer, "db_connection") and topicer.db_connection is not None and \
+                hasattr(topicer.db_connection, "connect") and hasattr(topicer.db_connection, "close"):
+            await topicer.db_connection.connect()
     yield
+
+    for topicer in app.state.loaded_topicers.values():
+        if hasattr(topicer, "db_connection") and topicer.db_connection is not None and \
+                hasattr(topicer.db_connection, "connect") and hasattr(topicer.db_connection, "close"):
+            await topicer.db_connection.close()
+
     logging.info("Shutting down TopicerAPI...")
 
 
